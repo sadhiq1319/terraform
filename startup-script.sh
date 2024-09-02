4b0f15f07bd577781256655dc0162e8eaebf4c96
sadhiq1319 <sadhiq1329@gmail.com>
2024-09-02 15:01:17 +0530

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Install Apache
-yum update -y
-yum install -y httpd
+sudo yum update -y
+sudo yum install -y httpd
 
 # Start Apache
-systemctl start httpd
-systemctl enable httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
 
 # Create a simple HTML page
 echo "<html><h1>Hello from Terraform!</h1></html>" > /var/www/html/index.html
